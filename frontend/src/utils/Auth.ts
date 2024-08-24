489b37ec7a0ee05f92e4d3ea19a8c1f739b0537a
Eduardo <eduardojunior010757@gmail.com>
2024-08-24 20:06:59 -0300

    create logOut

--- frontend/src/utils/Auth.ts
+++ frontend/src/utils/Auth.ts
@@ -4,3 +4,8 @@ const REFRESH_TOKEN = localStorage.getItem("refresh_token");
 export const handleGetAccessToken = () => localStorage.getItem(ACCESS_TOKEN ?? "");
 
 export const handleGetRefreshToken = () => localStorage.getItem(REFRESH_TOKEN ?? "");
+
+export const logOut = () => {
+    localStorage.removeItem(`${ACCESS_TOKEN}`);
+    localStorage.removeItem(`${REFRESH_TOKEN}`)
+}
