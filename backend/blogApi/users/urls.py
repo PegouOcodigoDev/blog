a1c01a2280f65a8fbd0afbf8719fa88ef496b66a
Eduardo <eduardojunior010757@gmail.com>
2024-09-09 18:38:19 -0300

    add logout

--- backend/blogApi/users/urls.py
+++ backend/blogApi/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from users.views.signup import SignUp
 from users.views.login import Login
+from users.views.logout import Logout
 from rest_framework_simplejwt.views import TokenRefreshView # type: ignore
 
 urlpatterns = [
     path("signup", SignUp.as_view()),
     path("login", Login.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
+    path('api/logout/', Logout.as_view()),
 ]
