e3b5883bee08826de4b88139120a5502fcf9f6bc
Eduardo <eduardojunior010757@gmail.com>
2024-09-09 18:44:13 -0300

    add allowany in permission class

--- backend/blogApi/users/views/logout.py
+++ backend/blogApi/users/views/logout.py
@@ -1,9 +1,13 @@
 from rest_framework_simplejwt.tokens import RefreshToken # type: ignore
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
 class Logout(APIView):
+    
+    permission_classes = [AllowAny]
+    
     def post(self, request):
         try:
             refresh_token = request.data["refresh"]
